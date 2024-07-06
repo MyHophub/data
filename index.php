63466055d1403702d293c025beae04ed90f83043
MyHophub <djbabu1230@gmail.com>
2024-07-06 22:38:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Pure CSS Responsive Navbar Example</title>
+
+    <meta name="author" content="Codeconvey" />
+    <!-- Navbar CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!--Only for demo purpose - no need to add.-->
+    <link rel="stylesheet" href="css/demo.css" />
+	
+</head>
+<body>
+	
+	
+	<!DOCTYPE html>
+<html>
+<body>
+
+
+<center>
+<form action="">
+<br>Link<br>
+  <input type="search" id="" name="A">
+<br> Name<br>
+  <input type="search" id="" name="B">
+ <br>img<br>
+  <input type="search" id="" name="C">
+ <br>Tag Name<br>
+  	
+ <select name="D">
+    <option value="HIGH QUALITY MOVIE">HIGH QUALITY MOVIE</option>
+    <option value="LOW QUALITY MOVIE">LOW QUALITY MOVIE</option>
+    <option value="VIP QUALITY MOVIE">VIP QUALITY MOVIE</option>
+    <option value="HD SOUND QUALITY">HD SOUND QUALITY</option>
+  </select>
+
+  <br><br><input type="submit">
+  </center>
+</form>
+<br><br>
+
+
+<?php if(isset($_GET['A'])) { $link = $_GET['A']; }?>
+<?php if(isset($_GET['B'])) { $name = $_GET['B']; }?>
+<?php if(isset($_GET['C'])) { $img = $_GET['C']; }?>
+<?php if(isset($_GET['C'])) { $tag = $_GET['D']; }?>
+
+
+<center><textarea id="cop"rows="10" cols="45">
+
+
+<tr><td><center><div class="block"><div class="cardk">
+<a href="/play.php?A=<?php echo $link ;?>&B=<?php echo $name ;?>&C=<?php echo $img ;?>&D=<?php echo $tag ;?>">
+<table><tr>
+<th><img src="<?php echo $img ;?>" width="110" height="110" alt=""></th>
+<td><b style="color: ;font-style: oblique;"><?php echo $name ;?></b><br>
+<b style="color:red"><i class="fa fa-folder-open" aria-hidden="true"></i> <?php echo $tag ;?></b>
+</tr></table></div></div></center></tr>
+
+
+</textarea><br><br><br>
+<button onclick="my()">Copy Data</button>
+<script> 
+
+function my() {
+ document.getElementById("cop").select();
+    document.execCommand("copy");
+
+  }
+  
+  </script>
+
+
+
+<br><br><br>
+<a href="/"> HOME PAGE </a>
+</center>
+
+
+	</body>
+</html>
